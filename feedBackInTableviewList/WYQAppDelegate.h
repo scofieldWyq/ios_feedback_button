35198ae0425ac822f8c8f253c278dcd10a5e6f77
scofieldWyq <147036scofield@gmail.com>
2015-04-28 23:13:13 +0800

    Initial Commit

---
+++ feedBackInTableviewList/WYQAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WYQAppDelegate.h
+//  feedBackInTableviewList
+//
+//  Created by 吴英强 on 15/4/28.
+//  Copyright (c) 2015年 吴英强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WYQAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ feedBackInTableviewList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  feedBackInTableviewList
+//
+//  Created by 吴英强 on 15/4/28.
+//  Copyright (c) 2015年 吴英强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WYQAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WYQAppDelegate class]));
+    }
+}
